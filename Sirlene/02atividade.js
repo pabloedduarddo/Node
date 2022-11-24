bed01a2a211a7598688040c99e2b7272a41d5f5a
Pablo Eduardo Moreira <95319664+pabloedduarddo@users.noreply.github.com>
2022-11-24 13:17:55 -0300

    Update 02atividade.js

--- Sirlene/02atividade.js
+++ Sirlene/02atividade.js
@@ -1,6 +1,6 @@
-let read = require('readline-sync')
+let read = require("readline-sync")
 
- let real = Number(read.question("dolar em real"))
- let dolar = (real * 5.36)
+let real = Number(read.question("Qual o valor em real? "))
+let dolar = (real * 5.36 )
 
- console.log(`${real} Dolar em real ${dolar.toFixed(2)}`)
+console.log(`${real}R$ em Dolar são ${dolar.toFixed(2)} Dolares`)
